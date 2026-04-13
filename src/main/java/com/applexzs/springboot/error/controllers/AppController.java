c29c6fdebecbc239e1eaa23e8fe04f6cd6c02633
Applexzs <reverseapplemw2@gmail.com>
2026-04-13 11:10:21 -0600

    NumberFormatException test

--- src/main/java/com/applexzs/springboot/error/controllers/AppController.java
+++ src/main/java/com/applexzs/springboot/error/controllers/AppController.java
@@ -10,7 +10,8 @@ public class AppController {
 
     @GetMapping("/app")
     public String index(){
-        int value = 100 / 0;
+        //int value = 100 / 0;
+        int value = Integer.parseInt("10x");
         System.out.println("value = " + value);
         return "ok 200";
     }
